9cceb5231f5e7cc3029a24ffc42eeb668a7d15e3
Deathmonster100 <49344412+Deathmonster100@users.noreply.github.com>
2019-04-06 20:11:52 -0400

    Create noob.js

---
+++ noob.js
@@ -0,0 +1,10 @@
+alert("Welcome to the South Brunswick Public Library Volunteering Website!")
+function submit(){
+	alert("Good Job you signed up for volunteering for either crafts, games or a scavenger hunt at the Children's Spring Fling on Sat Apr 27 from 11-2:30!")
+}
+function facepaint(){
+	alert("Good Job you signed up for volunteering for facepainting for the Children's Spring Fling on Sat Apr 27 from 11-2:30!")
+}
+function crafts(){
+	alert("Good Job you signed up for volunteering for craft preperations for the Children's Spring Fling on Sat Apr 27 from 11-2:30!")
+}
